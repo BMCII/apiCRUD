60f5fbe0e1b428ac081898c8a9d639f2ac38118b
Brian Cobb <brian.m.cobbii@fullsail.edu>
2016-09-12 14:30:52 -0400

    working on routs

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -1,14 +1,13 @@
 module.exports = (express) => {
   const router = express.Router();
 
-  router.get('/user', (req, res) => {
-    console.log("route hit", req.body);
+  router.get('/users', (req, res) => {
     res.json({
       healthy: true,
     })
   });
 
-  // router.use('/api/', require('./app/user')(express));
+   //router.use('/api/', require('./app/user')(express));
 
   return router;
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ module.exports = (express) => {
   const router = express.Router();
 
   router.post('/status', (req, res) => {
-    console.log("route hit", req.body);
     res.json({
       healthy: true,
     })
